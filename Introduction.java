6a7af06c4168bf7a410d3e6c7c9411e87b3e9283
Jerrick <jermil2990@gmail.com>
2025-08-30 17:21:42 -0500

    Hello World Script Test

--- Introduction.java
+++ Introduction.java
@@ -1,3 +1,12 @@
 public class Introduction {
-    
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        String message = "Hello";
+        System.out.println(message.toLowerCase());
+        message = message.replace('o','h');
+        System.out.println(message);
+      
+
+
+    }
 }
